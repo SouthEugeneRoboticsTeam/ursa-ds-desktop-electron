68e2d519d425cc09c456d23bbc58c876bc6e54b2
Aidan Heller <asfheller@gmail.com>
2019-07-08 18:44:40 -0700

    Initial commit

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,3 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+let h1 = document.createElement("h1");
+h1.appendChild(document.createTextNode("Hello, world!"));
+document.body.appendChild(h1);
